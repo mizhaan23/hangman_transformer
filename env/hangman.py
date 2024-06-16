7c86d1f532848f41abab263f5d8a16494cc82b54
Mizhaan Maniyar <104845333+mizhaan23@users.noreply.github.com>
2024-06-16 22:51:18 +0530

    aaa

--- env/hangman.py
+++ env/hangman.py
@@ -62,13 +62,13 @@ class HangmanEnv(gym.Env):
         self.dataset = shuffle(dataloader.dataset)
         self.counter = init_counter
         self.max_seq_len = max_seq_len
-        self.action_space = spaces.Discrete(26)  # 26 possible actions (a-z)
+        self.action_space = spaces.Discrete(28)  # 26 possible actions (a-z) + '' + '_'
         self.observation_space = spaces.Box(low=0, high=27, shape=(self.max_seq_len,), dtype=int)
 
         self.hidden_word = None
         self.word_length = None
         self._reset_attributes()
-
+    
     def _reset_attributes(self):
         self.guessed_letters = set()
         self.remaining_attempts = 6  # Maximum attempts
@@ -76,17 +76,22 @@ class HangmanEnv(gym.Env):
         self.game_over = False
 
     def reset(self, *, seed=0, options=None):
-        self.hidden_word = self.dataset[self.counter]
+        self.hidden_word = self.dataset[self.counter % len(self.dataset)]
         self.word_length = len(self.hidden_word)
         self._reset_attributes()
-
+        
         # Increment reset counter
         self.counter += 1
 
         current_word = ''.join([char if char in self.guessed_letters else '_' for char in self.hidden_word])
         self.current_state = self.word2state(current_word)
-        return self.current_state, {'word': current_word, 'hidden_word': self.hidden_word,
-                                    'guessed_letters': self.guessed_letters}
+        return self.current_state, \
+            {
+            'word': current_word, 
+            'hidden_word': self.hidden_word, 
+            'guessed_letters': self.guessed_letters,
+            'remaining_attempts': self.remaining_attempts,
+        }
 
     def generate_random_word(self):
         # Replace this with your logic for generating random words
@@ -112,9 +117,13 @@ class HangmanEnv(gym.Env):
 
         current_word = ''.join([char if char in self.guessed_letters else '_' for char in self.hidden_word])
         self.current_state = self.word2state(current_word)
-        return self.current_state, reward, self.game_over, self.game_over, {'word': current_word,
-                                                                            'hidden_word': self.hidden_word,
-                                                                            'guessed_letters': self.guessed_letters}
+        return self.current_state, reward, self.game_over, self.game_over, \
+            {
+            'word': current_word, 
+            'hidden_word': self.hidden_word, 
+            'guessed_letters': self.guessed_letters,
+            'remaining_attempts': self.remaining_attempts,
+        }
 
     def word2state(self, word):
         state = [27 if char == '_' else ord(char) - ord('a') + 1 for char in word]
